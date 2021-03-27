18fbe926bfc984369ae9803bc58748adc8faf7e0
patrick.premartin@developpeur-pascal.fr <Patrick>
2021-03-27 11:00:41 +0100

    initialisation du projet

---
+++ AnimationDroiteGauche/Unit1.pas
@@ -0,0 +1,24 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+
+type
+  TForm1 = class(TForm)
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
