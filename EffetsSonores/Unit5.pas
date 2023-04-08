f79464cb093594ef03d5ae1e9b195dc179bf725d
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-04-08 19:11:27 +0200

    updated use of TMusicLoop class for playing music and sounds

--- EffetsSonores/Unit5.pas
+++ EffetsSonores/Unit5.pas
@@ -40,7 +40,7 @@ procedure TForm5.Button1Click(Sender: TObject);
 begin
   if not assigned(mus1) then
   begin
-    mus1 := TMusicLoop.Create(Self);
+    mus1 := TMusicLoop.Create;
     mus1.load('..\..\..\assets\GSP_500_Noises\16AXEHOR.WAV');
   end;
   mus1.play('', false);
@@ -53,7 +53,7 @@ procedure TForm5.Button2Click(Sender: TObject);
 begin
   if not assigned(mus2) then
   begin
-    mus2 := TMusicLoop.Create(Self);
+    mus2 := TMusicLoop.Create;
     mus2.load('..\..\..\assets\GSP_500_Noises\16BEEROP.WAV');
   end;
   mus2.play(false);
@@ -66,7 +66,7 @@ procedure TForm5.Button3Click(Sender: TObject);
 begin
   if not assigned(mus3) then
   begin
-    mus3 := TMusicLoop.Create(Self);
+    mus3 := TMusicLoop.Create;
     mus3.load('..\..\..\assets\GSP_500_Noises\16BELSL1.WAV');
   end;
   mus3.PlaySound;
@@ -79,7 +79,7 @@ procedure TForm5.Button5Click(Sender: TObject);
 begin
   if not assigned(mus5) then
   begin
-    mus5 := TMusicLoop.Create(Self);
+    mus5 := TMusicLoop.Create;
     mus5.load('..\..\..\assets\GSP_500_Noises\16BURNER.WAV');
   end;
   mus5.PlaySound;
@@ -88,5 +88,21 @@ end;
 initialization
 
 ReportMemoryLeaksOnShutdown := true;
+
+mus1 := nil;
+mus2 := nil;
+mus3 := nil;
+mus5 := nil;
+
+finalization
+
+if assigned(mus1) then
+  mus1.Free;
+if assigned(mus2) then
+  mus2.Free;
+if assigned(mus3) then
+  mus3.Free;
+if assigned(mus5) then
+  mus5.Free;
 
 end.
