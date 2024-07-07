9a23cd412b2e411063873e9259371ea997d24434
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-07-07 18:42:33 +0200

    updated units links and a-upgraded project options to Delphi 12 Athens

--- AnimationGrossissement/AnimationGrossissement.dpr
+++ AnimationGrossissement/AnimationGrossissement.dpr
@@ -4,7 +4,7 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   Unit8 in 'Unit8.pas' {Form8},
-  Gamolf.FMX.MusicLoop in '..\lib-externes\FMXGameEngine\src\Gamolf.FMX.MusicLoop.pas' {MusicLoop: TDataModule};
+  Gamolf.FMX.MusicLoop in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.FMX.MusicLoop.pas';
 
 {$R *.res}
 
