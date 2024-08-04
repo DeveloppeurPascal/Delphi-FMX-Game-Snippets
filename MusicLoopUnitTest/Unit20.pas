4fbba8a1196699c35cb465fd2190bd1c3774713b
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-04 08:48:00 +0200

    added header copyright comments

--- MusicLoopUnitTest/Unit20.pas
+++ MusicLoopUnitTest/Unit20.pas
@@ -1,3 +1,48 @@
+/// <summary>
+/// ***************************************************************************
+///
+/// Delphi FMX Game Snippets
+///
+/// Copyright 2021-2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Examples of what is done when developing video games: sprite management,
+/// background music, sound effects, animations, ...
+///
+/// Projects are developed under Delphi with its FireMonkey multiplatform
+/// framework to run our projects under Windows, macOS, iOS, Android and Linux
+/// from the same code base.
+///
+/// Not all images and musics used in this repository are free of charge.
+/// Reuse them only if you have a license. They remain the property of their
+/// respective authors and are only present in the programs for demo purposes.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+///      Patrick PREMARTIN
+///
+/// Site :
+///      https://fmxgamesnippets.developpeur-pascal.fr
+///
+/// Project site :
+///      https://github.com/DeveloppeurPascal/Delphi-FMX-Game-Snippets
+///
+/// ***************************************************************************
+/// File last update : 07/07/2024 19:05:42
+/// Signature : f94ba7e6d8453e848385616c14917e7b62eb60af
+/// ***************************************************************************
+/// </summary>
+
 unit Unit20;
 
 interface
