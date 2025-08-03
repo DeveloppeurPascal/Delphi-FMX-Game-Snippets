760e6910da247069dba6efd506112f68f81a7224
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-08-03 19:49:34 +0200

    added Kenney.nl assets to have some ships in the BitmapBidouilles sample project

---
+++ BitmapBidouilles/assets/README.md
@@ -0,0 +1,8 @@
+# Kenney's game assets
+
+This project uses game assets from Kenney. If you want to use them in your projects, please download them from [kenney.nl](https://www.kenney.nl) website and support his work with a donation or [buying his "all assets" pack](https://kenney.itch.io/kenney-game-assets).
+
+The graphics used in this project come from these packs :
+
+* [Space shooter redux](https://www.kenney.nl/assets/space-shooter-redux)
+* [Space shooter extension](https://www.kenney.nl/assets/space-shooter-extension)
---
+++ BitmapBidouilles/assets/udmShips.pas
@@ -0,0 +1,78 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  Looping Animation Demo
+
+  Copyright 2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://developpeur-pascal.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/LoopingAnimationDemo
+
+  ***************************************************************************
+  File last update : 2025-07-20T15:57:17.952+02:00
+  Signature : 9048c511225a3a6b3c1c039d3266a1f9eb5f6dbf
+  ***************************************************************************
+*)
+
+unit udmShips;
+
+// ****************************************
+// * Images from folder :
+// * /Users/patrickpremartin/Downloads/LoopingAnimationDemo-assets/_/Ships
+// ****************************************
+//
+// This file contains a TDataModule with a 
+// TImageList to use in a FireMonkey project.
+//
+// ****************************************
+// File generator : Folder to FMX Image List v1.2
+// Website : https://folder2fmximagelist.olfsoftware.fr/
+// Generation date : 2025-07-20T15:57:17.951Z
+//
+// Don't do any change on this file.
+// They will be erased by next generation !
+// ****************************************
+
+interface
+
+uses
+  System.SysUtils, 
+  System.Classes, 
+  System.ImageList,
+  FMX.ImgList;
+
+type
+  TdmShips = class(TDataModule)
+    ImageList: TImageList;
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  dmShips: TdmShips;
+
+implementation
+
+{%CLASSGROUP 'FMX.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
